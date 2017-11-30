dd792b0a7144ee6319bbba077aad762b80b81f9d
Daniel Felipe Florez Salgado <danielflorez111@hotmail.com>
2017-11-29 20:02:04 -0500

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
+import { ItemComponent } from './components/items/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemComponent,
   ],
   imports: [
     BrowserModule
